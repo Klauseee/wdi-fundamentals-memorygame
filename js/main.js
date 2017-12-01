6fd9e3fe524ae83630b15c91393608d24ea7fa65
Nic Wilson <nicholaswilson3010@gmail.com>
2017-12-01 21:00:32 +0000

    tidied game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-console.log("up and running");
-
-
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards [0];
-var cardTwo = cards [1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("you found a match!");
-	} else {
-		alert("sorry, try again");
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("you found a match!");
+			} else {
+			alert("sorry, try again");
+		}
 	}
 }
+
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
